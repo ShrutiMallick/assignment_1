0b670ea5642b0c3b59eeedebb3bfc08bb556275e
ShrutiMallick <53283341+ShrutiMallick@users.noreply.github.com>
2019-08-25 19:45:25 +0530

    Add files via upload

---
+++ leapyear1.java
@@ -0,0 +1,9 @@
+public class Main{
+ public static void main(string[]args){
+int y=1999;
+if(y%4==0){
+System.out.println("the year is leap year");
+else 
+{System.out.println("the year is not leap year";)}
+         }
+}
