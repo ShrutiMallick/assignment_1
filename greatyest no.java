65271af06ea8fd7502846cbfa8a9f865262c9da5
ShrutiMallick <53283341+ShrutiMallick@users.noreply.github.com>
2019-08-25 21:26:37 +0530

    Add files via upload

---
+++ greatyest no.java
@@ -0,0 +1,14 @@
+public class greatest number{
+
+   	public static void main(String[]args){
+	int a=5,b=9,c=10,d=8;
+	if(a >b&&a>c&&a>d){
+	System.out.println("a is the greatest number");}
+	else if(b>c&&b>d){
+	System.out.println("b is the greatest number");}
+	else if(c>d){
+	System.out.println("c is the greatest number");}
+	else
+	{System.out.println("d is the greatest number");}
+        }
+}
