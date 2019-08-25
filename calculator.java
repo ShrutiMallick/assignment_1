519df004f8cdb92049e30aa73a706f715d4ca1e6
ShrutiMallick <53283341+ShrutiMallick@users.noreply.github.com>
2019-08-25 20:37:15 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,28 @@
+public class calculator{
+ 	public static void main(string[]args){
+	int a=5,b=9;
+
+	char operator;
+	
+	 operator='/';
+	 
+	int result;
+	int result;
+	switch(operator)
+		{
+		    case '+': result=a+b;
+    			System.out.println(result);
+		 break;
+		    case'-': result=a-b;
+			System.out.println(result);
+       		break;
+		    case'*':result=a*b;
+			System.out.println(result);
+		break;
+		   case'/':result=a/b;
+			System.out.println(result);
+		break;
+	         	  default:System.out.println("invalid operation");
+                               }
+	}
+ }
