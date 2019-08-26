e01412cb15167c84ca0c04adb0bff6219d3db412
ShrutiMallick <53283341+ShrutiMallick@users.noreply.github.com>
2019-08-26 19:36:07 +0530

    Update greatyest no.java

--- greatyest no.java
+++ greatyest no.java
@@ -1,14 +1,9 @@
-public class greatest number{
+public class greatestnumber{
 
    	public static void main(String[]args){
 	int a=5,b=9,c=10,d=8;
-	if(a >b&&a>c&&a>d){
-	System.out.println("a is the greatest number");}
-	else if(b>c&&b>d){
-	System.out.println("b is the greatest number");}
-	else if(c>d){
-	System.out.println("c is the greatest number");}
-	else
-	{System.out.println("d is the greatest number");}
-        }
+	int result;
+	result=(a>b?a:b);
+	result=(c>d?(c> result?c:result):(d>result?d:result));
+	System.out.println(result);}
 }
