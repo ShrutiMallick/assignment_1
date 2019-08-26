35bf08b7c87d936ef42a76a902ba95fb4111509b
ShrutiMallick <53283341+ShrutiMallick@users.noreply.github.com>
2019-08-26 19:21:33 +0530

    Update leapyear2.java

--- leapyear2.java
+++ leapyear2.java
@@ -1,13 +1,13 @@
-public class leapyear2{
+public class Main{
  	public static void main(String[]args){
 	int y=2016;
                  if (y%400==0)
 		{if (y%100!=0)
 			{System.out.println("year is leap year");}
 	else
-		{System.out.println("year is not a leap year")}
+		{System.out.println("year is not a leap year");}
 		}
 	else
-                          {System.out.println("year is leap year")}
+                          {System.out.println("year is leap year");}
 	}
  }
