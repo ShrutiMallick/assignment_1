19e98edecb48dda41d830707dfc27b2a8535e4d4
ShrutiMallick <53283341+ShrutiMallick@users.noreply.github.com>
2019-09-01 18:39:57 +0530

    Update eular2.java

--- eular2.java
+++ eular2.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[]args){
         int  a=0,b=1,c=0;
         
-        for(int i=2;i<400000;i+=2){
+        for(int i=2;i<4000000;i+=2){
             c=a+b;
             a=b;
             b=c;
