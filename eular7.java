7f5bc673ff9344565e142981debdf3512b6fbf73
ShrutiMallick <53283341+ShrutiMallick@users.noreply.github.com>
2019-09-02 20:09:02 +0530

    Update eular7.java

--- eular7.java
+++ eular7.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[]args){
         int  x=0,c=0,i=2;
         
-        while(x!=6){
+        while(x!=10001){
             for(int j=2;j<=i;j++){
                 if(i%j==0){
                   c=c+1;
@@ -12,13 +12,13 @@ public class Main{
                 {
                 x=x+1;
                 }
-                if(x==6)
+                if(x==10001)
                 {
-                System.out.println("Prime number at 1001st position is "+i);
+                System.out.println("Prime number at 10001st position is "+i);
                 
                 }
                 c=0;
                 i++;
             }
-            
+    }
             }
